fa581716160c5c07c38628c4407277c03ea5dbb4
Helkio <kmarten2004@hotmail.com>
2019-05-29 09:30:50 -0600

    Init commit

---
+++ Test (2).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    double num1;
+    double num2;
+    double sum;
+    cout << "enter your first number:";
+    cin >> num1;
+    cout << "enter your second number:";
+    cin >> num2;
+    sum=num1+num2;
+    cout << num1 << '+' << num2 << '=' << sum;
+    }
